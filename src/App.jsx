aea3688c145e24189af4381516055c54d0fc692c
Amzat19 <kunmiamzat1@gmail.com>
2023-08-05 13:08:55 +0100

    Initialized istart task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+
+      </header>
+    </div>
+  )
+}
+
+export default App
